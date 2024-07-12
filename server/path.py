4b08de7fe845315245e0e88086362a63111e2fda
ltrix07 <skotarenko138@gmail.com>
2024-07-12 12:37:40 +0300

    git pull

--- server/path.py
+++ server/path.py
@@ -1,6 +1,29 @@
 get_proxies_path = '/get_proxies'
+"""
+Params: None
+"""
+
 send_message_path = '/message'
+"""
+Params: text; chat_type
+"""
+
 error_attention_path = '/error_attention'
+"""
+Params: shop_name; message_text
+"""
+
 saving_and_sending_file_path = '/send_file'
+"""
+Params: chat_type; caption; file_name; file
+"""
+
 send_report_path = '/report'
+"""
+Params: chat_type; report; errors; amazon_status; repricer_status
+"""
+
 error_backend_path = '/error_backend'
+"""
+Params: error_text, shop_name
+"""
