bc9babb431dc282dda54307970ce4c219e662dc6
ltrix07 <skotarenko138@gmail.com>
2024-07-12 14:51:03 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="main_server_manager",
+    version="0.2.0",
+    description="Библиотека, которая используется в дополнении с Checker-Plus для взаимодействия чекера, поставщика "
+                "прокси и телеграм бота.",
+    author="ltrix07",
+    author_email="ltrix02@gmail.com",
+    url="https://github.com/ltrix07/Main-server-manager",
+    packages=find_packages(),
+    install_requires=requirements
+)
